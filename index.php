bf08b726a530a89251c4fc1f3d7c5150a85a86dd
Allison <73033057+abkelley01@users.noreply.github.com>
2021-03-06 09:08:51 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<! DOCTYPE html>
+<html lang ='en-US'>
+<head><title>Forum</title>
+<meta charset='UTF-8'>
+</head>
+
+<body>
+
+<form action='login.php' method='POST'>
+Username: <input type='text' name='username' placeholder='Username'/> <br/>
+Password: <input type='password' name='password' placeholder='Password'/><br/>
+<input type='submit' name='login' value='Log In'/>
+</form>
+
+
+</body>
+
+</html>
